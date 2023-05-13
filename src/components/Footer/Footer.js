799b9a0c0130b01d09a6de8d5e61fdfa68caa4c9
Leonid <leonid0991@gmail.com>
2023-05-13 19:30:55 +0300

    fix: noreferer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,21 +10,36 @@ export function Footer() {
       </p>
       <nav className="footer__navigation">
         <ul className="footer__navigation-list">
-          <a  className="footer__link" href="/">
+          <a className="footer__link" href="/">
             <li>Home</li>
           </a>
-          <a  className="footer__link" target="_blank" href="https://practicum.com/en-isr/">
+          <a
+            className="footer__link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://practicum.com/en-isr/"
+          >
             <li>Practicum</li>
           </a>
         </ul>
         <ul className="footer__navigation-list">
           <li>
-            <a  className="footer__link" target="_blank" href="https://github.com/leonid9191/">
+            <a
+              className="footer__link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/leonid9191/"
+            >
               <img src={github} alt="github" />
             </a>
           </li>
           <li>
-            <a  className="footer__link" target="_blank" href="https://www.facebook.com/leonid.chernobilskiy/">
+            <a
+              className="footer__link"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.facebook.com/leonid.chernobilskiy/"
+            >
               <img src={facebook} alt="facebook" />
             </a>
           </li>
